6a5b347b2b2e13bc83a3f294db0832f035597b11
Sykoj <jakubsykora@protonmail.com>
2018-07-21 22:53:26 +0200

    Change request date format in JSON response

--- TS-web-app/Model/TableauSolution.cs
+++ TS-web-app/Model/TableauSolution.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
 using Ts.IO;
 
 namespace TsWebApp.Model {
 
     public class TableauSolution {
 
+        [JsonProperty("solutionId")]
         public int SolutionId { get; set; }
+
+        [JsonProperty("tableauInput")]
         public TableauInput TableauInput { get; set; }
+
+        [JsonProperty("solutionNode")]
         public SolutionNode SolutionNode { get; set; }
+
+        [JsonProperty("requestDate")]
+        [JsonConverter(typeof(Iso8601DateConverter))]
         public DateTime RequestDateTime { get; set; }
+    }
+
+    internal class Iso8601DateConverter : JsonConverter {
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+
+            if (value is DateTime date) {
+                writer.WriteValue(date.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'", CultureInfo.InvariantCulture));
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+            return false;
+        }
+
+        public override bool CanConvert(Type objectType) {
+            return false;
+        }
     }
 }
