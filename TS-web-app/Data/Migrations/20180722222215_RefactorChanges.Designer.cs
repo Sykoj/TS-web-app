e06c06d1d28e1aa3bdb10baa2a60451c34d3c6ef
Sykoj <jakubsykora@protonmail.com>
2018-07-23 00:24:59 +0200

    Add migrations due to refactor

--- TS-web-app/Data/Migrations/20180722222215_RefactorChanges.Designer.cs
+++ TS-web-app/Data/Migrations/20180722222215_RefactorChanges.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ts.App.Data;
 
 namespace Ts.App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180722222215_RefactorChanges")]
+    partial class RefactorChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TS-web-app/Data/Migrations/20180722222215_RefactorChanges.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ts.App.Data.Migrations
+{
+    public partial class RefactorChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TableauSolutions");
+
+            migrationBuilder.CreateTable(
+                name: "TableauSerializedSolutions",
+                columns: table => new
+                {
+                    SolutionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TableauInputSerialized = table.Column<string>(nullable: true),
+                    SolutionNodeSerialized = table.Column<string>(nullable: true),
+                    RequestDateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableauSerializedSolutions", x => x.SolutionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TableauSerializedSolutions");
+
+            migrationBuilder.CreateTable(
+                name: "TableauSolutions",
+                columns: table => new
+                {
+                    SolutionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RequestDateTime = table.Column<DateTime>(nullable: false),
+                    SolutionNodeSerialized = table.Column<string>(nullable: true),
+                    TableauInputSerialized = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableauSolutions", x => x.SolutionId);
+                });
+        }
+    }
+}
